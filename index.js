2bf370ae20545d3359ab493f2ffe523308b4d778
Harsh Choudhary <harshvirat894@gmail.com>
2023-09-16 22:33:00 +0530

    ConnectionOfNodeAndMongodb

---
+++ index.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose";
+import Blog from "./model/Blog.js";
+import Login from "./model/login.js";
+
+mongoose.connect("mongodb+srv://harsh:harsh@harsh.2uxfgjs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+var myTitle = 'Awesome Post!';
+
+//Create a new blog post object
+//First method
+
+// const article = new Blog({
+//     _id: 1,
+//     title: myTitle,
+//     slug: 'awesome post slug',
+//     published: true,
+//     content: 'This is the best post ever',
+//     tags: ['featured', 'announcement'],
+// })
+
+// Insert a the article in your own mongoDB database
+
+// await article.save();
+
+//First method code ends
+
+const article = await Blog.create({
+    title: myTitle,
+    slug: 'awesome post slug',
+    published: true,
+    content: 'This is the best post ever',
+    tags: ['featured', 'announcement'],
+})
+
+// overwriting the title of article
+article.title = "The most";
+await article.save();
+
+
+
+console.log(article);
+
+const myLogin = await Login.create({
+    username: "harsh",
+    password: "Harsh",
+})
+
+
+console.log(myLogin);
